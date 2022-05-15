2c78dfea78ef54f12afb89a4a4c65200706a7577
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2022-05-15 14:23:09 +0300

    added mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 (() => {
   const menuBtn = document.getElementById('menu-btn')
+  const siteNav = document.getElementById('site-nav')
 
   menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('is-open')
+    siteNav.classList.toggle('is-open')
   })
 })()
