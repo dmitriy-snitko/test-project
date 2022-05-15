df1a04b33ea40ca4d8a740706bc56ac7d232e480
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2022-05-15 14:40:23 +0300

    scroll-lock

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
 (() => {
   const menuBtn = document.getElementById('menu-btn')
   const siteNav = document.getElementById('site-nav')
+  const body = document.querySelector('body')
 
   menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('is-open')
     siteNav.classList.toggle('is-open')
+    body.classList.toggle('is-open')
   })
 })()
